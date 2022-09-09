4592df27d630c81199baa0fe95b942946a33f707
Amaraaahh <amarachiochvba@gmail.com>
2022-09-08 17:26:18 -0700

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main (void)
+	/**
+	 * main - Entry point
+	 *results - Always 0 (sucess)
+	 */
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return(0);
 }
