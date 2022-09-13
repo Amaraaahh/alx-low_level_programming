c33f99e5ded55873d9fb871d6d9787e38b74c9be
Amaraaahh <amarachiochvba@gmail.com>
2022-09-13 12:57:44 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - print the alphabet, in lowercase, followed by a new line.
+ *
+ * Description: using the main function
+ * this program prints "programming prints the alphabet, in lowercase, followed by a new line
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char c;
+	
+	for(c = 'a'; c <= 'z'; c++);
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
