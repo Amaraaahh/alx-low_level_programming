e915850244c214ad27ca0a544fa683ca061ade72
Amaraaahh <amarachiochvba@gmail.com>
2022-09-13 11:49:56 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print _putchar, followed by a new line
+ *
+ * Description: using the main function
+ * this program prints " _putchar, followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
