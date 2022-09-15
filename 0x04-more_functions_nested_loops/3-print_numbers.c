42118e2ad6f9f31a50da1b39e972b68199ecbfc6
Amaraaahh <amarachiochvba@gmail.com>
2022-09-15 10:21:47 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main -  prints the numbers, from 0 to 9, followed by a new line
+ *
+ * Return: returns nothing
+ */
+
+void print_numbers(void)
+{
+	int h;
+
+	if (h >= 0; h <= 10; h++)
+	{
+		putchar (h);
+	}
+	putchar ('\n');
+}
